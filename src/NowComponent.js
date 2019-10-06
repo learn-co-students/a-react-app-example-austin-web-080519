27cf7a8f3bb55f162eb5871048e012313febe8fc
andrewrosario <rosario.andrew@gmail.com>
2019-10-06 18:08:17 -0500

    Done.

---
+++ src/NowComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+// no need to modify anything in here. The second test is looking for the returned JSX from this component
+class NowComponent extends Component {
+  render() {
+    return (
+        <header className="App-header">
+        NowComponent
+        </header>
+    );
+  }
+}
+
+export default NowComponent;
